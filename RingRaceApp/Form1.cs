37d9cc1505b21d8090e9cc7c20304f8890d621f9
EsahQ <kovalevvanc@gmail.com>
2025-04-24 13:14:00 +0300

    +better menu

--- RingRaceApp/Form1.cs
+++ RingRaceApp/Form1.cs
@@ -25,15 +25,19 @@ namespace RingRaceApp
         private PictureBox trackPreview;
         private PictureBox player1CarPreview;
         private PictureBox player2CarPreview;
+
+        private PictureBox player1_text;
+        private PictureBox player2_text;
+
         private Dictionary<string, Vector2[]> trackTextures = new Dictionary<string, Vector2[]>()
         {
             {
                 "sprites/road2.png", new Vector2[]
                 {
-                    new Vector2(895, 1080 / 4 - 100),
-                    new Vector2(895, 1080 / 4 - 200),
-                    new Vector2(928, 10),
-                    new Vector2(928, 269)
+                    new Vector2(895, 1080 / 4 - 100), // Спавн 1
+                    new Vector2(895, 1080 / 4 - 200), // Спавн 2
+                    new Vector2(928, 10), // Финиш 1 точка
+                    new Vector2(928, 269) // Финиш 2 точка
                 }
             },
             {
@@ -49,7 +53,7 @@ namespace RingRaceApp
         private List<string> trackKeys;
         private int currentTrackIndex = 0;
         private List<string> player1Cars = new List<string> { "sprites/car1_blue_menu.png", "sprites/car2_blue_menu.png" };
-        private List<string> player2Cars = new List<string> { "sprites/car1_red_menu.png"};
+        private List<string> player2Cars = new List<string> { "sprites/car1_red_menu.png", "sprites/car2_red_menu.png"};
         private int player1CarIndex = 0;
         private int player2CarIndex = 0;
         private GLControl glControl;
@@ -82,107 +86,192 @@ namespace RingRaceApp
 
         private void SetupMenuPanel()
         {
-            panelMenu = new Panel
+            panelMenu = new DoubleBufferedPanel
             {
                 Dock = DockStyle.Fill,
                 BackgroundImage = Image.FromFile("sprites/background_menu.png")
             };
+            Controls.Add(panelMenu);
 
-            btnStart.Size = new Size(640, 260);
-            btnStart.BackgroundImage = Image.FromFile("sprites/button_up1.png");
-            btnStart.Click += (s, e) => { ShowGame(); };
-
-            btnLeft.Size = new Size(144, 144);
-            btnLeft.BackgroundImage = Image.FromFile("sprites/button_left.png");
-            btnLeft.Click += BtnLeft_Click;
-
-            btnRight.Size = new Size(144, 144);
-            btnRight.BackgroundImage = Image.FromFile("sprites/button_right.png");
-            btnRight.Click += BtnRight_Click;
-
-            btnPlayer1Left.Size = new Size(144, 144);
-            btnPlayer1Left.BackgroundImage = Image.FromFile("sprites/button_left.png");
-            btnPlayer1Left.Click += BtnPlayer1Left_Click;
-
-            btnPlayer1Right.Size = new Size(144, 144);
-            btnPlayer1Right.BackgroundImage = Image.FromFile("sprites/button_right.png");
-            btnPlayer1Right.Click += BtnPlayer1Right_Click;
-
-            btnPlayer2Left.Size = new Size(144, 144);
-            btnPlayer2Left.BackgroundImage = Image.FromFile("sprites/button_left.png");
-            btnPlayer2Left.Click += BtnPlayer2Left_Click;
-
-            btnPlayer2Right.Size = new Size(144, 144);
-            btnPlayer2Right.BackgroundImage = Image.FromFile("sprites/button_right.png");
-            btnPlayer2Right.Click += BtnPlayer2Right_Click;
-
-            buttonList = new List<Button> { btnStart, btnLeft, btnRight, btnPlayer1Left, btnPlayer1Right, btnPlayer2Left, btnPlayer2Right };
-
-            trackPreview = new PictureBox
+            // 1) Внешний TLPanel: 3 колонки по 33%
+            var outer = new TableLayoutPanel
             {
-                Size = new Size(btnStart.Width, (int)(btnStart.Height * 1.3)),
-                SizeMode = PictureBoxSizeMode.StretchImage
+                Dock = DockStyle.Fill,
+                BackColor = Color.Transparent,
+                ColumnCount = 3,
+                RowCount = 1,
             };
+            outer.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33f));
+            outer.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33f));
+            outer.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33f));
+            panelMenu.Controls.Add(outer);
 
-            player1CarPreview = new PictureBox
-            {
-                Size = new Size(160, 320),
-                SizeMode = PictureBoxSizeMode.StretchImage,
-                BackColor = Color.Transparent
-            };
+            // 2) Создаем три «зоны»
+            var leftZone = CreatePlayerZone(isPlayer1: true);
+            var centerZone = CreateCenterZone();
+            var rightZone = CreatePlayerZone(isPlayer1: false);
 
-            player2CarPreview = new PictureBox
-            {
-                Size = new Size(160, 320),
-                SizeMode = PictureBoxSizeMode.StretchImage,
-                BackColor = Color.Transparent
-            };
+            outer.Controls.Add(leftZone, 0, 0);
+            outer.Controls.Add(centerZone, 1, 0);
+            outer.Controls.Add(rightZone, 2, 0);
 
-            UpdateTrackPreview();
-            UpdatePlayer1CarPreview();
-            UpdatePlayer2CarPreview();
-
-            foreach (Button btn in buttonList)
+            // Общая стилизация кнопок
+            foreach (var btn in new[] { btnStart, btnLeft, btnRight,
+                                 btnPlayer1Left, btnPlayer1Right,
+                                 btnPlayer2Left, btnPlayer2Right })
             {
                 btn.FlatStyle = FlatStyle.Flat;
                 btn.FlatAppearance.BorderSize = 0;
-                btn.BackgroundImageLayout = ImageLayout.Stretch;
+                btn.BackColor = Color.Transparent;
                 btn.FlatAppearance.MouseOverBackColor = Color.Transparent;
                 btn.FlatAppearance.MouseDownBackColor = Color.Transparent;
-                btn.BackColor = Color.Transparent;
-                panelMenu.Controls.Add(btn);
             }
-            panelMenu.Controls.Add(trackPreview);
-            panelMenu.Controls.Add(player1CarPreview);
-            panelMenu.Controls.Add(player2CarPreview);
 
-            panelMenu.Resize += (s, e) =>
-            {
-                btnStart.Location = new Point(
-                    (panelMenu.ClientSize.Width - btnStart.Width) / 2,
-                    (int)((panelMenu.ClientSize.Height - btnStart.Height) / 1.1)
-                );
-                btnLeft.Location = new Point(
-                    (panelMenu.ClientSize.Width - btnStart.Width) / 2,
-                    (int)((panelMenu.ClientSize.Height - btnStart.Height) / 1.5)
-                );
-                btnRight.Location = new Point(
-                    (panelMenu.ClientSize.Width + btnStart.Width) / 2 - btnRight.Width,
-                    (int)((panelMenu.ClientSize.Height - btnStart.Height) / 1.5)
-                );
-                trackPreview.Location = new Point(
-                    (panelMenu.ClientSize.Width - trackPreview.Width) / 2,
-                    (panelMenu.ClientSize.Height - trackPreview.Height) / 4
-                );
-                btnPlayer1Left.Location = new Point(panelMenu.ClientSize.Width / 16, (int)((panelMenu.ClientSize.Height - btnStart.Height) / 1.5));
-                btnPlayer1Right.Location = new Point((int)(panelMenu.ClientSize.Width / 16 + btnPlayer1Right.Width * 1.2), (int)((panelMenu.ClientSize.Height - btnStart.Height) / 1.5));
-                player1CarPreview.Location = new Point((int)(panelMenu.ClientSize.Width / 16 + btnPlayer1Right.Width / 1.85), (panelMenu.ClientSize.Height - trackPreview.Height) / 4);
-                btnPlayer2Left.Location = new Point((int)(panelMenu.ClientSize.Width * 15 / 16 - btnPlayer2Right.Width * 2 * 1.1), (int)((panelMenu.ClientSize.Height - btnStart.Height) / 1.5));
-                btnPlayer2Right.Location = new Point(panelMenu.ClientSize.Width * 15 / 16 - btnPlayer2Right.Width, (int)((panelMenu.ClientSize.Height - btnStart.Height) / 1.5));
-                player2CarPreview.Location = new Point((int)(panelMenu.ClientSize.Width * 15 / 16 - btnPlayer2Right.Width / 1.85 * 3.1), (panelMenu.ClientSize.Height - trackPreview.Height) / 4);
-            };
-            Controls.Add(panelMenu);
+            // Навешиваем обработчики
+            btnLeft.Click += BtnLeft_Click;
+            btnRight.Click += BtnRight_Click;
+            btnPlayer1Left.Click += BtnPlayer1Left_Click;
+            btnPlayer1Right.Click += BtnPlayer1Right_Click;
+            btnPlayer2Left.Click += BtnPlayer2Left_Click;
+            btnPlayer2Right.Click += BtnPlayer2Right_Click;
+            btnStart.Click += (s, e) => ShowGame();
         }
+
+        private TableLayoutPanel CreatePlayerZone(bool isPlayer1)
+        {
+            // создаём локально новые PictureBox’ы
+            var textBox = new PictureBox();
+            var carPreview = new PictureBox();
+
+            // сохраняем в поля, чтобы дальше их можно было использовать, и чтобы не было null
+            if (isPlayer1)
+            {
+                player1_text = textBox;
+                player1CarPreview = carPreview;
+            }
+            else
+            {
+                player2_text = textBox;
+                player2CarPreview = carPreview;
+            }
+
+            // получаем остальные параметры
+            var btnL = isPlayer1 ? btnPlayer1Left : btnPlayer2Left;
+            var btnR = isPlayer1 ? btnPlayer1Right : btnPlayer2Right;
+            var carList = isPlayer1 ? player1Cars : player2Cars;
+            var carIndex = isPlayer1 ? player1CarIndex : player2CarIndex;
+            var textSprite = isPlayer1 ? "sprites/player1_menu.png"
+                                       : "sprites/player2_menu.png";
+
+            // Вертикальный TLPanel: 4 строки
+            var tl = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                RowCount = 5,
+                ColumnCount = 1,
+            };
+            tl.RowStyles.Add(new RowStyle(SizeType.Percent, 10f));
+            tl.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            tl.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            tl.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            tl.RowStyles.Add(new RowStyle(SizeType.Percent, 17f));
+
+            // 1) textBox
+            textBox.Image = Image.FromFile(textSprite);
+            textBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            textBox.Size = new Size(600, 100);
+            textBox.Dock = DockStyle.Top;
+            tl.Controls.Add(textBox, 0, 1);
+
+            // 2) carPreview
+            Image image = Image.FromFile(carList[carIndex]);
+            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            carPreview.Image = image;
+            carPreview.SizeMode = PictureBoxSizeMode.CenterImage;
+            carPreview.Size = new Size(160, 320);
+            carPreview.Dock = DockStyle.Top;
+            carPreview.Margin = new Padding(0, 10, 0, 10);
+            tl.Controls.Add(carPreview, 0, 2);
+
+
+            // 3) кнопки внизу
+            var flow = new FlowLayoutPanel
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                Dock = DockStyle.Bottom,
+                AutoSize = true,
+                Anchor = AnchorStyles.Bottom,
+                Padding = new Padding(0, 0, 0, 20)
+            };
+            btnL.Size = new Size(144, 144);
+            btnR.Size = new Size(144, 144);
+            btnL.BackgroundImage = Image.FromFile("sprites/button_left.png");
+            btnR.BackgroundImage = Image.FromFile("sprites/button_right.png");
+            btnL.BackgroundImageLayout = ImageLayout.Stretch;
+            btnR.BackgroundImageLayout = ImageLayout.Stretch;
+            flow.Controls.Add(btnL);
+            flow.Controls.Add(btnR);
+            tl.Controls.Add(flow, 0, 3);
+
+            return tl;
+        }
+
+
+        private TableLayoutPanel CreateCenterZone()
+        {
+            // 1) Сначала создаём сам PictureBox и сохраняем в поле
+            trackPreview = new PictureBox();
+
+            // 2) Теперь делаем TableLayoutPanel с 4 строками
+            var tl = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                RowCount = 4,
+                ColumnCount = 1,
+            };
+            tl.RowStyles.Add(new RowStyle(SizeType.Percent, 100f));
+            tl.RowStyles.Add(new RowStyle(SizeType.AutoSize));      // trackPreview
+            tl.RowStyles.Add(new RowStyle(SizeType.AutoSize));      // кнопки L/R
+            tl.RowStyles.Add(new RowStyle(SizeType.AutoSize));      // btnStart
+
+            // 5) Кнопка старта
+            btnStart.Size = new Size(630, 260);
+            btnStart.BackgroundImage = Image.FromFile("sprites/button_up.png");
+            btnStart.BackgroundImageLayout = ImageLayout.Stretch;
+            btnStart.Dock = DockStyle.Top;
+            btnStart.Margin = new Padding(0, 0, 0, 20);
+            tl.Controls.Add(btnStart, 0, 3);
+
+            // 4) Кнопки влево/вправо
+            var flowLR = new FlowLayoutPanel
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                AutoSize = true,
+                Dock = DockStyle.Top,
+                Anchor = AnchorStyles.Top,
+                Margin = new Padding(0, 0, 0, 20)
+            };
+            btnLeft.Size = new Size(144, 144);
+            btnRight.Size = new Size(144, 144);
+            btnLeft.BackgroundImage = Image.FromFile("sprites/button_left.png");
+            btnRight.BackgroundImage = Image.FromFile("sprites/button_right.png");
+            btnLeft.BackgroundImageLayout = ImageLayout.Stretch;
+            btnRight.BackgroundImageLayout = ImageLayout.Stretch;
+            flowLR.Controls.Add(btnLeft);
+            flowLR.Controls.Add(btnRight);
+            tl.Controls.Add(flowLR, 0, 2);
+
+            // 3) Настраиваем trackPreview
+            trackPreview.SizeMode = PictureBoxSizeMode.StretchImage;
+            trackPreview.Size = new Size(btnStart.Width, (int)(btnStart.Height * 1.3));
+            trackPreview.Dock = DockStyle.Top;
+            trackPreview.Margin = new Padding(0, 20, 0, 20);
+            trackPreview.Image = Image.FromFile(trackKeys[currentTrackIndex]);
+            tl.Controls.Add(trackPreview, 0, 1);
+
+            return tl;
+        }
+
 
         private void BtnLeft_Click(object sender, EventArgs e)
         {
